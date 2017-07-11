4c0e480776bb47673942634cbaf18445f83d89f3
TurboBasic <off@boun.cr>
2017-07-11 16:46:19 +0300

    Powershell: startup scripts progression

--- powershell/Modules/Environment/GPO scripts/bbro-mao-logon.ps1
+++ powershell/Modules/Environment/GPO scripts/bbro-mao-logon.ps1
@@ -1,5 +1,6 @@
 # User Logon script %systemRoot%\System32\GroupPolicy\User\Scripts\Logon\bbro-mao-logon.ps1 
 
+
   #region     initialization
 
       # Default Log filename for Write-Log
@@ -18,12 +19,19 @@
 
   #endregion
 
+
+
+
   "`n[{0,-14} {1}]" -f 'header user', (Set-LogEntry) | Out-String | Write-Log
 
   'User logon script {0,-22}, full path: {1}' -f 
         (Split-Path $PSCommandPath -Leaf), $PSCommandPath | 
         Write-Log
+
   Send-NetMessage "User logon script $PSCommandPath"
+
+
+
 
   $__user_variables = @{ 
 
@@ -65,49 +73,15 @@
     ubuntu =                 '%LOCALAPPDATA%\lxss\rootfs'
   }
 
+
+
   Import-Environment -Environment $__user_variables -Scope User
+
+
+
 
   "`n[{0,-14} {1}]" -f 'body user', (Set-LogEntry) | Out-String | Write-Log
   Get-ChildItem ENV: | 
       Out-String -Width 2048 -Stream | 
       ForEach { $_.TrimEnd() } |
       Write-Log
-
-  #region extra information
-
-      <#region Powershell variables based on Environment vars  (__Set-GlobalVariables.ps1)
-        $__profileDir=              $env:psProfileDir
-        $__projects=                $env:projects
-        $__userName=                $env:USERNAME
-        $__homeDrive=               $env:HOMEDRIVE
-      #endregion#>  
-      <#region perl settings
-        perlPath=                  '%git%\usr\bin\core_perl', 
-                                   '%git%\usr\bin\site_perl', 
-                                   '%git%\usr\bin\vendor_perl' -join ';' 
-      #endregion#>
-      <#region Extra bits for future use
-        ..nodePath=                '%LOCALAPPDATA%\yarn\config\global\node_modules\.bin'
-        PATH=                      '%LOCALAPPDATA%\yarn\config\global\node_modules\.bin'
-        PATH=                      'C:\program files\imageMagick-7.0.5-Q16'
-        PATH=                      '%LOCALAPPDATA%\Microsoft\WindowsApps'       
-      #endregion#>
-      <#region User Base variables         
-        USERNAME=                  'mao'                     
-        HOMEDRIVE=                 'C:'
-        HOMEPATH=                  '%profiles%\%USERNAME%'
-        USERPROFILE=               '%HOMEDRIVE%%HOMEPATH%'
-        USERAPP=                   '%USERPROFILE%\APPDATA'
-        APPDATA=                   '%USERAPP%\roaming'
-        LOCALAPPDATA=              '%USERAPP%\local'
-        LOGONSERVER=               '\\ASUS'
-        USERDOMAIN=                'ASUS'
-        USERDOMAIN_ROAMINGPROFILE= 'ASUS'
-      #endregion#>
-      <#region Default User variables
-        PATH=                      '%USERPROFILE%\appData\local\microsoft\windowsApps';
-        TEMP=                      '%USERPROFILE%\appData\local\temp';
-        TMP=                       '%USERPROFILE%\appData\local\temp';
-      #endregion#>
-
-  #endregion
--- powershell/Modules/Environment/GPO scripts/bbro-startup.ps1
+++ powershell/Modules/Environment/GPO scripts/bbro-startup.ps1
@@ -1,6 +1,8 @@
 # Machine startup script %systemRoot%\System32\GroupPolicy\Machine\Scripts\Startup\bbro-startup.ps1 
 
+
   #region     initialization
+
 
       # Default Log filename for Write-Log
       $PSDefaultParameterValues = @{
@@ -8,21 +10,29 @@
               "${ENV:systemROOT}\System32\LogFiles\Startup, Shutdown, Logon scripts\StartupLogon.log"     
       }
 
+
       #TODO d: or e:
       $subModulePath =     'e:/0projects/dotfiles.windows/powershell/Modules/Environment/include'
       $commandModulePath = 'e:/0projects/dotfiles.windows/powershell/Modules/Commands/include'
+
 
       . "$subModulePath/Import-Environment.ps1"
       . "$commandModulePath/Set-LogEntry.ps1"
       . "$commandModulePath/Write-Log.ps1"
 
+
   #endregion
+
+
 
   "`n[{0,-14} {1}]" -f 'header machine', (Set-LogEntry) | Out-String | Write-Log
 
   'Machine startup script {0,-22}, full path: {1}' -f 
         (Split-Path $PSCommandPath -Leaf), $PSCommandPath | 
         Write-Log
+
+
+
 
   $__sys_variables = @{
     '..homeDrive' =             'C:'
@@ -52,16 +62,17 @@
     chocolateyToolsLocation =   '%..tools%'
 
     githubApi =                 'https://api.github.com'
-    git =                       '%COMMONPROGRAMFILES%\..\git'
-    git_Install_Root =          '%COMMONPROGRAMFILES%\..\git'
+    git =                       '%..programFiles%\git'
+    git_Install_Root =          '%..programFiles%\git'
 
-    gitPath =                   '%COMMONPROGRAMFILES%\..\git\cmd',
-                                '%COMMONPROGRAMFILES%\..\git',
-                                '%COMMONPROGRAMFILES%\..\git\mingw64\bin',
-                                '%COMMONPROGRAMFILES%\..\git\usr\bin' -join ';'
+    '..programFiles' =          '%..homeDrive%\Program Files'
+    gitPath =                   '%..programFiles%\git\cmd',
+                                '%..programFiles%\git',
+                                '%..programFiles%\git\mingw64\bin',
+                                '%..programFiles%\git\usr\bin' -join ';'
 
-    kdiff3 =                    '%COMMONPROGRAMFILES%\..\kdiff3'
-    'notepad++' =               '%COMMONPROGRAMFILES%\..\Notepad++\notepad++.exe'
+    kdiff3 =                    '%..programFiles%\kdiff3'
+    'notepad++' =               '%..programFiles%\Notepad++\notepad++.exe'
     '..userRoot' =              '\users' 
 
     '..scoopGlobal'=            '%ALLUSERSPROFILE%\scoop'
@@ -70,9 +81,10 @@
     TEMP=                       '%systemROOT%\temp'
     TMP=                        '%systemROOT%\temp'
 
-    junkPath =                  '%COMMONPROGRAMFILES(X86)%\..\skype\phone',
-                                '%COMMONPROGRAMFILES(X86)%\..\brackets\command',
-                                '%COMMONPROGRAMFILES%\..\microsoft SQL Server\130\tools\binn',
+    '..programFilesX86' =       '%..homeDrive%\Program Files (x86)'
+    junkPath =                  '%..programFilesX86%\skype\phone',
+                                '%..programFilesX86%\brackets\command',
+                                '%..programFiles%\microsoft SQL Server\130\tools\binn',
                                 '%ALLUSERSPROFILE%\oracle\java\javapath' -join ';'
 
     PATH =                      '%..systemBin%',
@@ -85,62 +97,14 @@
                                 '%gitPath%' -join ';'
   }
 
+
+
   Import-Environment -Environment $__sys_variables -Scope Machine
+
+
 
   "`n[{0,-14} {1}]" -f 'body machine', (Set-LogEntry) | Out-String | Write-Log
   Get-ChildItem ENV: | 
       Out-String -Width 2048 -Stream | 
       ForEach { $_.TrimEnd() } | 
       Write-Log
-
-  #region extra information
-
-      <#region Extra bits for future use
-        conEmuDir=                '%cmder%\vendor\conemu-maximus5'
-        java_Home=                '%PROGRAMFILES%\java\jdk1.8.0_121' 
-        laragon=                  'C:\laragon'
-        php_Ini_Scan_Dir=         '%laragon%\bin\php\current\ext'
-        phpstorm_Jdk=             '%java_Home%'
-        PATH=                     '%PROGRAMFILES%\docker\docker\resources\bin'
-        PATH=                     '%PROGRAMFILES(X86)%\nVidia Сorporation\physX\common'
-      #endregion#>
-      <#region System Base variables         
-        ALLUSERSPROFILE=          'C:\programData'
-        COMMONPROGRAMFILES=       '%PROGRAMFILES%\common files'
-        COMMONPROGRAMFILES(X86)=  '%PROGRAMFILES(X86)%\common files'
-        COMPUTERNAME=             'ASUS'
-        COMSPEC=                  '%systemROOT%\system32\cmd.exe'
-        PROGRAMDATA=              '%systemDRIVE%\programData'
-        PROGRAMFILES=             '%systemDRIVE%\program files'
-        PROGRAMFILES(X86)=        '%PROGRAMFILES% (x86)'
-        PROGRAMW6432=             '%PROGRAMFILES%'
-        PUBLIC=                   '%userFolder%\public'
-        systemDRIVE=              'C:'
-        systemROOT=               '%systemDRIVE%\windows'
-      #endregion#>
-      <#region Default Machine variables
-        COMSPEC=                  '%systemROOT%\system32\cmd.exe'
-        NUMBER_OF_PROCESSORS=     '4'
-        OS=                       'Windows_NT'
-        PROCESSOR_ARCHITECTURE=   'AMD64'
-        PROCESSOR_IDENTIFIER=     'Intel64 Family 6 Model 76 Stepping 3, GenuineIntel'
-        PROCESSOR_LEVEL=          '6'
-        PROCESSOR_REVISION=       '4c03'
-        PATH=                     '%systemROOT%\system32',
-                                  '%systemROOT%',
-                                  '%systemROOT%\system32\wbem',
-                                  '%systemROOT%\system32\windowsPowerShell\v1.0' - join ';'                                  
-        PATH=%PATH% +             'C:\program files (x86)\brackets\command',
-                                  'C:\program files\microsoft SQL Server\130\tools\binn',
-                                  'C:\program files (x86)\skype\phone',
-                                  'C:\programData\oracle\java\javapath' - join ';'                                 
-        PATHEXT=                  '.com;.exe;.bat;.cmd;.vbs;.vbe;.js;.jse;.wsf;.wsh;.msc'                                 
-        psModulePath=             '%PROGRAMFILES%\windowsPowerShell\modules',
-                                  '%systemROOT%\system32\windowsPowerShell\v1.0\modules' - join ';'                                  
-        TEMP=                     '%systemROOT%\temp'
-        TMP=                      '%systemROOT%\temp'
-        USERNAME=                 'SYSTEM'
-        WINDIR=                   '%systemROOT%'
-      #endregion#>
-
-  #endregion extra information
