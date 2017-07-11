19955869817967f2ece98677c1ccc8ea5ee25be3
TurboBasic <off@boun.cr>
2017-07-11 16:27:07 +0300

    Powershell: Profile DONE, Startup/Logon scripts DONE, Env Vars DONE

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,84 +1,61 @@
 ### Version: 0.3.0
 #~~~Requires -Modules Environment
 
-#region Public functions declaration
+  #region Localized messages
 
-  Function Set-Localisation([String]$language='en-US') {
-  <#
-      Localization attributes:
-        $Global:__currentLanguage  - active language
-        $Global:__defaultLanguage  - default embedded language
-        $Global:__messages         - hashtable with all localized messages for end-user
-
-      General usage (after initialization of __messages):
-        $__messages.messageCode -f messageParameters, ... | Write
-
-  #>
-
-    #region Localized messages
-
-      $assets = DATA -supportedCommand ConvertFrom-Json {
-        ConvertFrom-Json -InputObject @'
-          {
-            "languages":  [
-              "en-US",
-              "uk-UA"
-            ],
-            "messages": {
-                "uk-UA":  {
-                    "moduleSuccess":       "УСПІХ: {0} завантажено",
-                    "moduleLoading":       "Завантаження: {0}",
-                    "localisation":        "Підключається локалізація {0} ...",
-                    "moduleFailure":       "ПОМИЛКА: Неможливо знайти {0} ...",
-                    "exit":                "До побачення!",
-                    "errorLocalisation":   "Мову з кодом {0} не знайдено, буде використано мову {1}",
-                    "welcome":             "Початок роботи скріпту профіля користувача ..."
-                },
-                "en-US":  {
-                    "moduleSuccess":       "SUCCESS: {0} loaded",
-                    "moduleLoading":       "Loading: {0}",
-                    "localisation":        "Localisation {0} loading...",
-                    "moduleFailure":       "FAILURE: Sorry, no {0} found...",
-                    "exit":                "Good-bye!",
-                    "errorLocalisation":   "Language {0} not found, using {1} instead",
-                    "welcome":             "Entering User profile script $profile ..."
-                }
-            }
+    $assets = DATA -supportedCommand ConvertFrom-Json {
+      ConvertFrom-Json -InputObject @'
+        {
+          "languages":  [
+            "en-US",
+            "uk-UA"
+          ],
+          "messages": {
+              "uk-UA":  {
+                  "moduleSuccess":       "УСПІХ: {0} завантажено",
+                  "moduleLoading":       "Завантаження: {0}",
+                  "localisation":        "Підключається локалізація {0} ...",
+                  "moduleFailure":       "ПОМИЛКА: Неможливо знайти {0} ...",
+                  "exit":                "До побачення!",
+                  "errorLocalisation":   "Мову з кодом {0} не знайдено, буде використано мову {1}",
+                  "welcome":             "Початок роботи скріпту профіля користувача ...",
+                  "globalVarsError":     "Глобальні змінні не визначені -- {0} не знайдено.\nСкріпти, модулі та інші компоненти не працюватимуть"
+              },
+              "en-US":  {
+                  "moduleSuccess":       "SUCCESS: {0} loaded",
+                  "moduleLoading":       "Loading: {0}",
+                  "localisation":        "Localisation {0} loading...",
+                  "moduleFailure":       "FAILURE: Sorry, no {0} found...",
+                  "exit":                "Good-bye!",
+                  "errorLocalisation":   "Language {0} not found, using {1} instead",
+                  "welcome":             "Entering User profile script profile ...",
+                  "globalVarsError":     "Global Variables are not set -- {0} not found.\nScripts, modules and other stuff may not work"
+              }
           }
-'@
-      }
+        }
+'@    }
 
-    #endregion
-
-    Set-Variable __defaultLanguage -Scope Global -Value $assets.languages[0]        # en-US
-    Set-Variable __currentLanguage -Scope Global -Value $assets.languages[0]
-    New-Variable __messages        -Scope Global
-
-    $isDefault = $language -eq $Global:__defaultLanguage
-    $isValid   = $language -in $assets.languages
-    $Global:__messages = $assets.messages.$Global:__currentLanguage
-
-    if ($isValid) {
-      $Global:__currentLanguage = $language
-    } else {
-      $Global:__messages.errorLocalisation -f $language, $Global:__currentLanguage | Write-Error
+    $GlobalVarScript = DATA {
+      '/Modules/Environment/include/Set-UserGlobalVariables.ps1'
     }
 
-    $Global:__messages.localisation -f $Global:__currentLanguage | Write-Verbose
+  #endregion
 
-  }    # Function Set-Localisation
 
+
+#region Public functions declaration
 
   Function Copy-AllModules {
-    $From = Convert-Path "$__projects/dotfiles.windows/Powershell/Modules"
-    $To = Convert-Path "$__profileDir/Modules"
-    $ExcludeFolderMatch = '.git'
-    Write-Verbose $From
-    Write-Verbose $To
+    $From = "$__projects/dotfiles.windows/Powershell"
+    $To =    $__profileDir
+    $ExcludeFolder = '.git'
 
-    Copy-Tree -From $From -To $To -ExcludeFolderMatch $ExcludeFolderMatch
+    $What = @{From="$From/Modules";     To="$To/Modules";     Files=,'*'; XD=$ExcludeFolder },
+            @{From="$From/_profiles";   To="$To/_profiles";   Files=,'*'; XD=$ExcludeFolder },
+            @{From="$From/ISE_profile"; To="$To/ISE_profile"; Files=,'*'; XD=$ExcludeFolder },
+            @{From=$From; To=$To; Files='*profile.ps1', '*.json', 'Powershell.xml', '_install*.*'; XD='*' }
 
-    # robocopy $__projects/dotfiles.windows/powershell/Modules $__profileDir/Modules /MIR /FFT /Z /XA:H /W:5
+    $What | ForEach { robocopy $_.From  $_.To  $_.Files /MIR /FFT /Z /XA:H /W:5 /XD $_.XD }
   }
 
 
@@ -105,75 +82,63 @@
         Get-Variable | Format-Table -Property Name, Options -Autosize
       }
 
-      'loading functions...' | Write-Verbose
-
-  }       # $newFunctions
+  }
 
 
   $newAliases = {
-    'Creating aliases...' | Write-Verbose
     New-Alias 7zpath New-7zpath                        -Force -Scope Global
     New-Alias ginfo  Get-InfoVariables                 -Force -Scope Global
     New-Alias gist   Get-GithubGistApiUrlOfCurrentUser -Force -Scope Global
   }
 
-
-Function Set-Profile {
-  #region local functions declarations
-  #endregion
-
-  Copy-AllModules
-
-  . $newFunctions
-  . $newAliases
-
-}
-
 #endregion
+
+
+
 
 
 #region Execution
 
+
+  if( -Not(Get-Command Initialize-Localization -EA 0) ) {
+    Import-module Environment
+  }
+  Initialize-Localization (ConvertTo-Hashtable $assets)
   Set-Localisation 'uk-UA'
+
+
+
   Write-Host $PSVersionTable.PSVersion.ToString()
   Write-Host $__messages.welcome
 
+
+
   #region Set Global Variables
 
-<#
-    separate Machine and User variables:  Here should be $Global: variables only!
-    Machine ENV: variables and environment should be already set up in Machine Startup script
-    User    ENV: variables and environment should be already set up in User Logon script
+    & {
+        $ok = $true
+        $newPath = Join-Path (Split-Path $profile -Parent) $GlobalVarScript
+        $ok = $ok -And (Test-Path $newPath)
 
-    Proposed Environment setting procedure execution order:
+        & $newPath
 
-    1) Machine Startup Scripts:
-
-          %systemRoot%\System32\GroupPolicy\Machine\Scripts\Startup\bbro-startup.ps1
-
-    2) User Logon Scripts:
-
-          %systemRoot%\System32\GroupPolicy\User\Scripts\Logon\bbro-mao-logon.ps1
-
-    3) Powershell profile when running Powershell
-
-          - Set $Global: variables based on ENV: Machine, User and Volatile variables
-
-#>
-
-    $err = $False
-    '/Modules/Environment/include/Set-UserGlobalVariables.ps1' |
-        ForEach { Join-Path (Split-Path $profile -Parent) $_ } |
-        Where { ($err = Test-Path $_) | Out-Null ; $_ } |
-        ForEach { & $_ ; $err = $err -and $? }
-
-    if( $err ) {
-      Write-Warning 'Global Variables are not set -- Set-UserGlobalVariables.ps1 not found.'
-      Write-Warning "Scripts, modules and other stuff may not work"
+        $ok = $ok -And $?
+        if( !$ok ) {
+          Write-Warning $__messages.GlobalVarsError -f (Split-Path $GlobalVarScript -Leaf)
+        }
     }
 
   #endregion
 
-  Set-Profile
+
+
+
+  Copy-AllModules
+  Import-Module posh-git
+
+. $newFunctions
+. $newAliases
+
+
 
 #endregion
--- powershell/Modules/Environment/Environment.psm1
+++ powershell/Modules/Environment/Environment.psm1
@@ -1,6 +1,6 @@
 #region initialization of module -- dot source the individual scripts that make-up this module
 
-  <# 
+  <#
     $paths = @('include')
 
     Foreach ($path in $paths) {
@@ -8,8 +8,8 @@
       ForEach {
           . $_.ProviderPath
           Write-Verbose "$_.ProviderPath successfully included"
-      } 
-    } 
+      }
+    }
   #>
 
   Write-Host -ForegroundColor Green "Module $(Split-Path $PSScriptRoot -Leaf) was successfully loaded."
@@ -41,5 +41,70 @@
 
 
 
+$Script:__localizationAssets = ConvertFrom-Json -InputObject '{
+  "languages":  [
+    "en-US"
+  ],
+  "messages": {
+      "en-US":  {}
+  }
+}'
 
 
+Function Initialize-Localization {
+  #   Data structure for localized messages is self-explanatory
+      PARAM(
+          [PARAMETER( Mandatory )]
+          [VALIDATENOTNULLOREMPTY()]
+          [Hashtable]
+          $Data
+      )
+
+  $Script:__localizationAssets.messages = $Data | ConvertTo-JSON | ConvertFrom-Json
+
+  if($Data.messages) {
+    if($Data.messages.'en-US') {
+      # $Script:__localizationAssets.messages.'en-US' = $Data.messages.'en-US' | ConvertTo-JSON | ConvertFrom-Json
+      $Script:__localizationAssets.messages | Add-Member -Name 'en-US' -Type NoteProperty -Value ($Data.messages.'en-US' | ConvertTo-JSON | ConvertFrom-Json)
+      #$Script:__localizationAssets.messages.Add('en-US', ($Data.messages.'en-US' | ConvertTo-JSON | ConvertFrom-Json) )
+    } else {
+      $Data.messages.GetEnumerator() |
+        ForEach { $Script:__localizationAssets.messages.add($_.Key, ($_.Value | ConvertTo-JSON | ConvertFrom-Json) ) }
+    }
+  }
+
+  $Data.languages |
+      Where { $_ -notlike 'en-US'} |
+      ForEach { $Script:__localizationAssets.languages += $_ }
+}
+
+
+Function Set-Localisation([String]$language='en-US') {
+  <#
+      Localization attributes:
+        $Global:__currentLanguage  - active language
+        $Global:__defaultLanguage  - default embedded language
+        $Global:__messages         - hashtable with all localized messages for end-user
+
+      General usage (after initialization of __messages):
+        $__messages.messageCode -f messageParameters, ... | Write
+
+  #>
+
+  Set-Variable __defaultLanguage -Scope Global -Value $Script:__localizationAssets.languages[0]        # en-US
+  Set-Variable __currentLanguage -Scope Global -Value $Script:__localizationAssets.languages[0]
+  New-Variable __messages        -Scope Global
+
+  $isDefault = $language -eq $Global:__defaultLanguage
+  $isValid   = $language -in $Script:__localizationAssets.languages
+  $Global:__messages = $Script:__localizationAssets.messages.$Script:__currentLanguage
+
+  if ($isValid) {
+    $Global:__currentLanguage = $language
+  } else {
+    $Global:__messages.errorLocalisation -f $language, $Global:__currentLanguage | Write-Error
+  }
+
+  $Global:__messages.localisation -f $Global:__currentLanguage | Write-Verbose
+
+}
